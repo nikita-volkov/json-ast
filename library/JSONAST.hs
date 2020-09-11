2265c5a0a814d0d6f80b05fe53af2d53f39b3341
Nikita Volkov <nikita.y.volkov@mail.ru>
2020-09-11 10:45:47 +0300

    Change the naming convention

--- library/JSONAST.hs
+++ library/JSONAST.hs
@@ -1,7 +1,7 @@
 -- |
 -- This module contains no namespace conflicts with Prelude,
 -- so it can be imported unqualified.
-module JSONAST where
+module JsonAst where
 
 import Prelude
 import Data.Data (Data, Typeable)
@@ -12,17 +12,17 @@ import Data.Scientific (Scientific)
 
 
 -- |
--- A JSON value AST.
+-- JSON value abstract syntax tree.
 -- 
 -- Note that this datastructure is identical to \"aeson\" Value.
 -- Until \"aeson\" implements the dependency on \"json-ast\",
 -- you can use 'Unsafe.Coerce.unsafeCoerce' to work with it,
 -- thus sidestepping the redundant conversions.
-data JSON =
-  JSON_Object !(HashMap Text JSON) |
-  JSON_Array !(Vector JSON) |
-  JSON_String !Text |
-  JSON_Number !Scientific |
-  JSON_Bool !Bool |
-  JSON_Null
+data Json =
+  ObjectJson !(HashMap Text Json) |
+  ArrayJson !(Vector Json) |
+  StringJson !Text |
+  NumberJson !Scientific |
+  BoolJson !Bool |
+  NullJson
   deriving (Eq, Read, Show, Typeable, Data)
